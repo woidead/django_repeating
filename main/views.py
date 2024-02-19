c43059af2ba4b97e1ea34501a1d1f0924baa2f8a
woidead <abikbest05@gmail.com>
2024-02-19 16:11:38 +0600

    added sign in

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -18,5 +19,6 @@ def index(request):
 def contact(request):
     return render(request, 'main/contact.html')
 
+@login_required
 def profile_view(request):
     return render(request, 'main/profile.html')
